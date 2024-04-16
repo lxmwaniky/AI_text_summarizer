b2e05412f71fdfb173b8454170708e0d1eb682e7
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-16 13:09:56 +0300

    Update server.js and index.html files

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,13 @@ const app = express();
 
 const PORT = 3000
 
-app.use(express.json);
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.static('../public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/'));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}/`);
 })
